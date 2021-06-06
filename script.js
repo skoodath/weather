c4294ef1c13473075284329387fe78c39d90a087
Shiju Nambiar <>
2021-06-06 16:46:36 +0530

    change weather

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //populate date on top
-window.addEventListener('load', ()=>{
-    let day = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const showDate = () =>{
+    let day = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
     const cweek = document.querySelector('.weather__week');
     const cdate = document.querySelector('.weather__date');
     //const ctime = document.querySelector('.weather__time');
@@ -10,10 +10,8 @@ window.addEventListener('load', ()=>{
         month: "short",
         day: "numeric",
         year: "numeric"
-    });
-    //ctime.innerHTML = `${cdte.getHours()}:${cdte.getMinutes()}`;
-
-});
+        });
+    }
 
 //declare base url and api key for api call
 
@@ -31,38 +29,29 @@ window.addEventListener('load', ()=>{
         
         let urlWeather = `${api.baseurl}weather?q=${citInput.value}&appid=${api.key}&units=imperial`;
         let weatherdesc = document.querySelector('.weather__desc');
-
         if(event.keyCode === 13) {
 
-            fetch(urlWeather)
-            //axios.get(urlWeather)
-            .then (response => response.json())
-            .then(data => showWeather(data) )
-            .catch(error => {
-            if(error.response){
-                let errorMessage = error.response.data.message;
-                showError(errorMessage);
-                }
-            });
-            /*axios.get(urlForecast)
+            axios.get(urlWeather)
                 .then(response => {
-                    console.log(response.data);
-                    showForecast(response.data)
+                    showWeather(response.data)
                 })
                 .catch(error => {
                     if(error.response){
-                console.log(error.response.data.message);
-                    }
-            });*/
+                    console.log(error.response.data.message);
+                    showError(error.response.data.message);
+                }
+                
+            })
             citInput.value = "";
-            //tempinput.value ="";
-            //tempinput.classList.remove("phone__type_show");
-            }
-        
         }
-
+        
+    }
 //function to display weather details on the page
     const showWeather = (weatherData) => {
+        if(weatherData.cod === 200){
+            showDate();
+        }
+        let citInput = document.querySelector(".weather__cityinput input");
         let ccity = document.querySelector('.weather__city_inner');
         let ctemp = document.querySelector('.weather__cel');
         let ftemp = document.querySelector('.weather__fah');
@@ -80,49 +69,25 @@ window.addEventListener('load', ()=>{
         ctemp.innerHTML = `${celc}&deg;C`;
         ftemp.innerHTML = `${fht}&deg;F`;
         let iconstring = weatherData.weather[0].icon;
-        /* if (iconstring.charAt(iconstring.length-1) === "d"){
-            cdesc.classList.add("weather__desc_bgdark");
-            cmisc.classList.add("weather__misc_bgdark");
-            //fcst.classList.add('weather__forecast_bgdark')
-
-        } else if(iconstring.charAt(iconstring.length - 1) === "n"){
-            cdesc.classList.add("weather__desc_bglite");
-            cmisc.classList.add("weather__misc_bglite");
-            //fcst.classList.add('weather__forecast_bglite');
-        }*/
         cicon.innerHTML = `<img src="https://openweathermap.org/img/wn/${iconstring}@2x.png"
                                 alt ="${weatherData.weather[0].description}">`;
         cdesc.innerHTML = weatherData.weather[0].description;
         rlfl.classList.add("weather__real_style");
         rlfl.innerHTML = `Real Feel: ${c_rlfl}&deg;C &#124; ${f_rlfl}&deg;F`;
+        citInput.blur();
+    }
 
-}
-//function to display forecast details on the page
-/* const showForecast = (forecast) => {
-    let dtlist = forecast.list;
-    let dtlistItem = document.querySelectorAll('.forecast__dt');
-    let temfield = document.querySelector('.forecast__right');
-        temfield.innerHTML = "";
-        console.log(forecast.list);
-        } */
-
-
-//error message display function
 
 const showError = (msg) => {
-    const input = document.querySelector('.weather__cityinput input');
     let clearAll = document.querySelectorAll('.clear');
     clearAll.forEach(el => {
         el.innerHTML = "";
     });
-
     let errDis = document.querySelector('#errordisplay');
     errDis.classList.add("weather__error_hilite");
     errDis.innerHTML = `${msg}`;
-    input.classList.add('input__error');
     setTimeout(() => {
         errDis.classList.remove("weather__error_hilite");
-        input.classList.remove('input__error');
         errDis.innerHTML = ""
     }, 3000);
 }
@@ -130,43 +95,3 @@ const showError = (msg) => {
 //event listener to make the api call
 
 citInput.addEventListener("keypress", getWeather);
-
-//switch button to swap between forecast and weather
-
-const themeSwitch = document.querySelector('.weather__mode');
-const currentTheme = localStorage.getItem('theme');
-const defaultLabel = document.querySelector('.weather__modebox label');
-
-if (currentTheme){
-    document.documentElement.setAttribute('data-theme', currentTheme);
-    if (currentTheme === 'lite'){
-        themeSwitch.checked = true;
-        defaultLabel.innerHTML = `Dark`;
-    } else {
-        defaultLabel.innerHTML = `Lite`;
-    }
-}
-
-
-
-const themeSwitcher = (e) => {
-    let label = document.querySelector('.weather__modebox label');
-    let theme = document.querySelector('html');
-    if(e.target.checked){
-
-        theme.setAttribute('data-theme', 'lite');
-        localStorage.setItem('theme', 'lite');
-        label.innerHTML = 'Dark';
-
-    }
-    else if (!e.target.checked){
-
-        theme.setAttribute('data-theme', 'dark');
-        localStorage.setItem('theme', 'dark');
-        label.innerHTML = 'Lite';
-    
-    }
-
-}
-themeSwitch.addEventListener('change', themeSwitcher, false);
-
